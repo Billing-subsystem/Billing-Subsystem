61efa9672c6e2ad0cd1bf62c54da288e96de292c
Brandon Doan <77133856+BDoan10@users.noreply.github.com>
2026-03-20 18:28:48 -0700

    Update subscriptionPurchase.java

--- subscriptionPurchase.java
+++ subscriptionPurchase.java
@@ -1,16 +1,22 @@
-class SubscriptionPurchase {
+class SubscriptionPurchase extends Purchase {
+
     private int subscriptionID;
 
     // Default Constructor
-    SubscriptionPurchase() {}
+    SubscriptionPurchase() {
+        super(); // calls Purchase default constructor
+    }
 
     // Parameterized Constructor
-    SubscriptionPurchase(int subscriptionID) {
+    SubscriptionPurchase(String description, double amount, String date, int subscriptionID) {
+        super(description, amount, date);
         this.subscriptionID = subscriptionID;
     }
 
     // Accessor
-    public int getSubscriptionID() { return subscriptionID; }
+    public int getSubscriptionID() {
+        return subscriptionID;
+    }
 
     // Modifier
     public void setSubscriptionID(int subscriptionID) {
