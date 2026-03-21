d0d13fe6ea257e429822a5fbd90481975f2e8804
Brandon Doan <77133856+BDoan10@users.noreply.github.com>
2026-03-20 18:19:43 -0700

    Create subscription.java

---
+++ subscription.java
@@ -0,0 +1,38 @@
+
+class Subscription {
+    
+    private static int numOfSubscriptions = 0;
+    private int subscriptionID;
+    private double amount;
+    private String status;
+    private String frequency;
+    private String initialDate;
+
+    // Default Constructor
+    Subscription() {
+        this.subscriptionID = numOfSubscriptions++;
+        this.amount = 0;
+    }
+
+    // Parameterized Constructor
+    Subscription( double amount, String status, String frequency, String initialDate) {
+        this.subscriptionID = numOfSubscriptions++;
+        this.amount = amount;
+        this.status = status;
+        this.frequency = frequency;
+        this.initialDate = initialDate;
+    }
+
+    // Accessors
+    public int getSubscriptionID() { return subscriptionID; }
+    public double getAmount() { return amount; }
+    public String getStatus() { return status; }
+    public String getFrequency() { return frequency; }
+    public String getInitialDate() { return initialDate; }
+
+    // Modifiers
+    public void setAmount(double amount) { this.amount = amount; }
+    public void setStatus(String status) { this.status = status; }
+    public void setFrequency(String frequency) { this.frequency = frequency; }
+    public void setInitialDate(String initialDate) { this.initialDate = initialDate; }
+}
